77be51ed2fe5b1f70270c94fc1dcc83ef72532ad
antonio481 <adanilovx@gmail.com>
2020-10-11 20:48:55 +0400

    Commit change email

--- src/test/java/SimbirsoftTest/ProfilePage.java
+++ src/test/java/SimbirsoftTest/ProfilePage.java
@@ -43,7 +43,7 @@ public class ProfilePage {
     public void inputRecipientField() {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.visibilityOf(recipientField));
-        recipientField.sendKeys("noreply@github.com");
+        recipientField.sendKeys("farit.valiahmetov@simbirsoft.com");
     }
 
     public void clickWriteButton() {
